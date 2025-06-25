6fc34d7c90aeb1ef37b55a78503b19438177ffa2
IDevBro <mirsharipov.m98@gmail.com>
2025-06-25 13:50:57 +0500

    first commit

---
+++ components/layout/Footer.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div className="bg-yellow-500">
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ components/layout/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="bg-red-500">
+      <h1>Sidebar</h1>
+    </div>
+  );
+};
+
+export default Sidebar;
