d77b253f3eab0edf80c16ec2882404160062094b
azimjonxatamov <azimjonxatamov6@gmail.com>
2025-06-29 18:25:16 +0500

    Admin

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -3,7 +3,17 @@ import React from "react";
 const Admin = () => {
   return (
     <div>
-      <h1>Admin</h1>
+      <h1 className="font-bold">ADMINLAR - 2ta</h1>
+      <div className="flex pl-2.5 pr-2.5 justify-between mt-10 items-center shadow-xl h-15 ">
+        <p>Azamat Abduraimov </p>
+        <p>+99899 999 99 99</p>
+        <p>Uchtepa</p>
+      </div>
+      <div className="flex pl-2.5 pr-2.5 justify-between mt-10 items-center shadow-xl h-15 ">
+        <p>Azamat Abduraimov </p>
+        <p>+99899 999 99 99</p>
+        <p>Uchtepa</p>
+      </div>
     </div>
   );
 };
