ae06bb4ea2feeeac7a666ac18fb67db563853284
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2025-06-29 12:02:36 +0500

    branch creted

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Sidebar = () => {
   return (
     <div className="bg-red-500">
-      <h1>Sidebar</h1>
+      <h1 className="text-2xl">Sidebar</h1>
     </div>
   );
 };
